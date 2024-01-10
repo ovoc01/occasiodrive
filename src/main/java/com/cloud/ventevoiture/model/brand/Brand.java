748be9d81596834069cf8408c8f8c61777ec3dcf
tokyRaselison <tokyraselison2@gmail.com>
2024-01-10 23:02:37 +0300

    add  brand based module

---
+++ src/main/java/com/cloud/ventevoiture/model/brand/Brand.java
@@ -0,0 +1,30 @@
+package com.cloud.ventevoiture.model.brand;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id_brand;
+    String brand;
+
+    public int getId_brand() {
+        return id_brand;
+    }
+
+    public void setId_brand(int id_brand) {
+        this.id_brand = id_brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
---
+++ src/main/java/com/cloud/ventevoiture/model/repository/BrandRepository.java
@@ -0,0 +1,15 @@
+package com.cloud.ventevoiture.model.repository;
+
+import com.cloud.ventevoiture.model.brand.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface BrandRepository extends JpaRepository<Brand, String> {
+    @Query(value = "Select * from brand where id_brand = :id_brand" ,nativeQuery = true)
+    public Brand findOne(@Param("id_brand") int id_brand);
+
+    @Query(value = "delete from brand where id_brand = :id_brand", nativeQuery = true)
+    public void deleteById_brand(@Param("id_brand") int id_brand);
+
+}
