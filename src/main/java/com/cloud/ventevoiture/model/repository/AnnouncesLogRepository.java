aea35eba0b5d6cc4dc5b375972f4ad34aefdd402
ovoc01 <118508678+ovoc01@users.noreply.github.com>
2024-01-29 14:04:54 +0300

    adding controller for deleting and selling announces

--- src/main/java/com/cloud/ventevoiture/model/repository/AnnouncesLogRepository.java
+++ src/main/java/com/cloud/ventevoiture/model/repository/AnnouncesLogRepository.java
@@ -1,7 +1,11 @@
 package com.cloud.ventevoiture.model.repository;
 
 import com.cloud.ventevoiture.model.entity.announces.AnnouncesLog;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface AnnouncesLogRepository extends JpaRepository<AnnouncesLog,Integer> {
+
+   Optional<AnnouncesLog> findByIdAnnounce(Integer idAnnounce);
 }
