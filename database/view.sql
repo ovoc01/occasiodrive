046150ca2e9c1ee0dc82c20a50619cc2f0a573d4
ovoc01 <118508678+ovoc01@users.noreply.github.com>
2024-01-29 10:54:46 +0300

    dumping database script

--- database/view.sql
+++ database/view.sql
@@ -1,21 +1,23 @@
---view pour recuperer tout les puissance d'un modèle
+SELECT mt.id_motorisation_transmission,
+       mt.id_motorisation,
+       t.id_transmission,
+       t.name
+FROM motorisation_transmission mt
+         JOIN transmission t ON mt.id_transmission = t.id_transmission
 
 
-create or replace view v_motorisation_transmission as
-    select mt.id_motorisation_transmission,mt.id_motorisation,t.*
-        from motorisation_transmission mt
-        join public.transmission t on mt.id_transmission = t.id_transmission
-
-create or replace view v_motorisation_version as
-    select mv.id_motorisation_version,mv.id_motorisation,v.id_version,v.intitule,mv.details
-        from
-            motorisation_version mv
-            join public.version v on mv.id_version = v.id_version
+SELECT mft.id_motorisation_fuel_type,
+       mft.id_motorisation,
+       ft.id_fuel_type,
+       ft.label
+FROM motorisation_fuel_type mft
+         JOIN fuel_type ft ON ft.id_fuel_type = mft.id_fuel_type
 
 
-
-create or replace view v_motorisation_fuel_type as
-    select mft.id_motorisation_fuel_type,mft.id_motorisation,ft.*
-        from
-            motorisation_fuel_type mft
-        join public.fuel
+SELECT mv.id_motorisation_version,
+       mv.id_motorisation,
+       v.id_version,
+       v.intitule,
+       mv.details
+FROM motorisation_version mv
+         JOIN version v ON mv.id_version = v.id_version
