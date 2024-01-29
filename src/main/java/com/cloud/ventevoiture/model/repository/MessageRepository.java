cb912bd6bf6efb66fb4fd9abc699e755e2414304
ovoc01 <118508678+ovoc01@users.noreply.github.com>
2024-01-29 08:18:27 +0300

    fixing application.yml identation

--- src/main/java/com/cloud/ventevoiture/model/repository/MessageRepository.java
+++ src/main/java/com/cloud/ventevoiture/model/repository/MessageRepository.java
@@ -3,13 +3,12 @@ package com.cloud.ventevoiture.model.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.cloud.ventevoiture.model.entity.messages.Message;
 
-@Repository
+
 public interface MessageRepository extends MongoRepository<Message,ObjectId>{
     @Query("{'id_sender': ?0, 'id_receiver': ?1}")
     List<Message> findByIdSenderAndIdReceiver(Integer senderId, Integer receiverId);
