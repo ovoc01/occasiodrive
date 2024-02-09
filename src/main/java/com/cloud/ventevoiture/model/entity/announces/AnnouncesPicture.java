ac00c013acb48aff8ea2d90ae619d9031b07bfd0
ovoc01 <118508678+ovoc01@users.noreply.github.com>
2024-02-10 01:40:59 +0300

    yes

--- src/main/java/com/cloud/ventevoiture/model/entity/announces/AnnouncesPicture.java
+++ src/main/java/com/cloud/ventevoiture/model/entity/announces/AnnouncesPicture.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -31,6 +33,7 @@ public class AnnouncesPicture {
     Announce announce;
 
     @JsonIgnore
+    @Column(name = "imagebyte")
     byte[] imageByte;
 
     @JsonIgnoreProperties("pictures")
